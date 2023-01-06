51b8f6ccedc35041b74227be2f705de27985d959
rdr2211 <108988103+rdr2211@users.noreply.github.com>
2023-01-06 18:54:22 +0530

    Create Question03.cpp

---
+++ GoldmanSachs/Question03.cpp
@@ -0,0 +1,49 @@
+/*
+3. Factorial Trailing Zeroes
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+Example 3:
+
+Input: n = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= n <= 104
+*/
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        if(n <= 4) return 0;
+        
+        int count = 0;
+        for(int i = 5; i <= n; i++){
+            if(i % 5 == 0){
+                int temp = i;
+                while(temp % 5 == 0){
+                    count++;
+                    temp /= 5;
+                }
+            }
+            
+        }
+
+        return count;
+    }
+};
