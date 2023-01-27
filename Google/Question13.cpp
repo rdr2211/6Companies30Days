9ee9270ca59c6b19ba3c4714ce1eb75a79c3f639
rdr2211 <108988103+rdr2211@users.noreply.github.com>
2023-01-27 17:53:11 +0530

    Create Question13.cpp

---
+++ Google/Question13.cpp
@@ -0,0 +1,73 @@
+/*
+Company :- Google
+13. Swim in Rising Water
+
+You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
+
+The rain starts to fall. At time t, the depth of the water everywhere is t. You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+
+Return the least time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0).
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,2],[1,3]]
+Output: 3
+Explanation:
+At time 0, you are in grid location (0, 0).
+You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
+You cannot reach point (1, 1) until time 3.
+When the depth of water is 3, we can swim anywhere inside the grid.
+Example 2:
+
+
+Input: grid = [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+Output: 16
+Explanation: The final route is shown.
+We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+ 
+
+Constraints:
+
+n == grid.length
+n == grid[i].length
+1 <= n <= 50
+0 <= grid[i][j] < n2
+Each value grid[i][j] is unique.
+*/
+class Solution {
+public:
+    // My Intuition was to apply backtracking but run time exceeded, instead I used BFS + priority queue
+    bool chk(int i, int j, int n){
+        return i>=0 && i<n && j>=0 && j<n;
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<vector<int>>v(n, vector<int>(n, 1e9));
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>q;
+        q.push({grid[0][0], {0, 0}});
+        v[0][0]=grid[0][0];
+        while(!q.empty()){
+            int cnt=q.top().first, x=q.top().second.first, y=q.top().second.second;
+            q.pop();
+            int c[4]={-1, 0, 1, 0};
+            int r[4]={0, -1, 0, 1};
+            for(int i=0 ;i<4; i++){
+                int row=r[i]+x, col=c[i]+y;
+                if(chk(row, col, n)){
+                    if(cnt<grid[row][col]){
+                        v[row][col]=grid[row][col];
+                        q.push({v[row][col], {row, col}});
+                    }
+                    else if(cnt<v[row][col]){
+                        v[row][col]=cnt;
+                        q.push({v[row][col], {row, col}});
+                    }
+                }
+            }
+        }
+        return v[n-1][n-1];
+    }
+};
