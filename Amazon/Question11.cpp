21148ae7927267cf830d567c9323a4ee8017a0c1
rdr2211 <108988103+rdr2211@users.noreply.github.com>
2023-01-29 19:25:03 +0530

    Update Question11.cpp

--- Amazon/Question11.cpp
+++ Amazon/Question11.cpp
@@ -32,17 +32,19 @@ Constraints:
 
 1 <= startPos, endPos, k <= 1000
 */
-int dp[1001][1001] = {}, mod = 1000000007;
 class Solution {
 public:
-int dfs(int k, int d) {
-    if (d >= k)
-        return d == k;
-    if (dp[k][d] == 0)
-        dp[k][d] = (1 + dfs(k - 1, d + 1) + dfs(k - 1, abs(d - 1))) % mod;
-    return dp[k][d] - 1;
-} 
-int numberOfWays(int startPos, int endPos, int k) {
-    return dfs(k, abs(startPos - endPos));
-}
+    int dp[1001][1001] = {0}, mod = 1e9 + 7;
+    int helper(int k, int d){
+        if(d > k) return 0;
+        else if(d == k) return 1;
+        if(dp[k][d] == 0){
+            dp[k][d] = (1 + helper(k - 1, d + 1) + helper(k - 1, abs(d - 1))) % mod;
+        }
+        return dp[k][d] - 1;
+    }
+    int numberOfWays(int startPos, int endPos, int k) {
+        int ans = helper(k, abs(startPos - endPos));
+        return ans;
+    }
 };
