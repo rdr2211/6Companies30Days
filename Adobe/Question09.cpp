6b39c21486517b8f1447eca7aca9794bc47c9f6a
rdr2211 <108988103+rdr2211@users.noreply.github.com>
2023-01-31 01:52:35 +0530

    Create Question09.cpp

---
+++ Adobe/Question09.cpp
@@ -0,0 +1,65 @@
+/*
+Company :- Adobe
+9. Count Nodes Equal to Average of Subtree
+
+Given the root of a binary tree, return the number of nodes where the value of the node is equal to the average of the values in its subtree.
+
+Note:
+
+The average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+A subtree of root is a tree consisting of root and all of its descendants.
+ 
+
+Example 1:
+
+
+Input: root = [4,8,5,0,1,null,6]
+Output: 5
+Explanation: 
+For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 + 1 + 6) / 6 = 24 / 6 = 4.
+For the node with value 5: The average of its subtree is (5 + 6) / 2 = 11 / 2 = 5.
+For the node with value 0: The average of its subtree is 0 / 1 = 0.
+For the node with value 1: The average of its subtree is 1 / 1 = 1.
+For the node with value 6: The average of its subtree is 6 / 1 = 6.
+Example 2:
+
+
+Input: root = [1]
+Output: 1
+Explanation: For the node with value 1: The average of its subtree is 1 / 1 = 1.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 1000
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int, int> helper(TreeNode* root, int &ans) {
+        // Intuition :- To count the sum of the nodes int the subtree as well as count no. nodes in the subtree.
+        if(root == NULL) return {0, 0};
+
+        pair<int, int> a = helper(root -> left, ans);
+        pair<int, int> b = helper(root -> right, ans);
+
+        if(root -> val == (root -> val + a.first + b.first)/(1 + a.second + b.second)) ans++;
+        return {root -> val + a.first + b.first, 1 + a.second + b.second};
+    }
+    int averageOfSubtree(TreeNode* root) {
+        int ans = 0;
+        helper(root, ans);
+        return ans;
+    }
+};
